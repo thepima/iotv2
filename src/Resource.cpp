b446c54ad872944a30d75bdb6a5fc0b03ceba238
thepima <joaquincarranza1234@gmail.com>
2026-09-01 15:25:10 -0500

    docs: update README

--- src/Resource.cpp
+++ src/Resource.cpp
@@ -3,10 +3,9 @@
 #include <string_view>
 
 /**
- *
- * @param name
- * @param type
- * @retur n
+ * @brief Constructor for the Resource with the given name and type.
+ * @param name Unique name of the resource.
+ * @param type Type of the resource (Consumable or Usable).
  */
 Resource::Resource(std::string_view name, Type type)
 : name(name), isAvailable(true), resourceType(type) {}
@@ -14,15 +13,16 @@ Resource::Resource(std::string_view name, Type type)
 /**
  * @brief Returns the name of the resource.
  *
- * @return A non
+ * @return A non-owning view of the resource's name.
  */
 std::string_view Resource::getName() const {
     return name;
 }
 
 /**
+ * @brief Retrieves the type of the resource.
  *
- * @return
+ * @return The type of the resource (Consumable or Usable).
  */
 Type Resource::getResourceType() const {
     return resourceType;
